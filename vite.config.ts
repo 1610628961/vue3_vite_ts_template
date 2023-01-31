7d5c0880c0ab634c9a0653b2ee23e6ff1bc3d0ff
kehan <kehan@fanmiot.cn>
2023-01-31 16:20:21 +0800

    build: 添加ant-design-vue,组件按需引入插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
-	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': resolve('src'),
+			'#': resolve('types'),
+		},
+	},
+	plugins: [
+		vue(),
+
+		// 组件按需引入
+		Components({
+			extensions: ['vue'],
+			resolvers: [AntDesignVueResolver()],
+			include: [/\.vue$/, /\.vue\?vue/],
+			dts: 'types/components.d.ts',
+		}),
+	],
 })
