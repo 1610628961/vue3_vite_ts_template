38daeb2734a53be0b774cdefdab0ba4ffb003cb8
kehan <kehan@fanmiot.cn>
2023-03-06 16:20:56 +0800

    feat: 给环境变量取值添加类型断言

--- src/enums/env_enum.ts
+++ src/enums/env_enum.ts
@@ -1,7 +1,7 @@
-export enum EnvEnum {
-	baseUrl = import.meta.env.VITE_BASE_URL,
-	apiBaseUrl = import.meta.env.VITE_API_BASE_URL,
-	indexUrl = import.meta.env.VITE_INDEX_URL,
-	proxyUrl = import.meta.env.VITE_PROXY_BASE_URL,
-	socketUrl = import.meta.env.VITE_SOCKET_BASE_URL,
+export const EnvEnum = {
+	baseUrl: import.meta.env.VITE_BASE_URL as unknown as string,
+	apiBaseUrl: import.meta.env.VITE_API_BASE_URL as unknown as string,
+	indexUrl: import.meta.env.VITE_INDEX_URL as unknown as string,
+	proxyUrl: import.meta.env.VITE_PROXY_BASE_URL as unknown as string,
+	socketUrl: import.meta.env.VITE_SOCKET_BASE_URL as unknown as string,
 }
