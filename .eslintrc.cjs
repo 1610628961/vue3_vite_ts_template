45b6cd79affe599e6582ca63b4b765b89ed57912
kehan <kehan@fanmiot.cn>
2023-04-03 17:49:16 +0800

    build: 添加eslint 依赖

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		'vue/setup-compiler-macros': true,
+	},
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'@vue/eslint-config-typescript/recommended',
+		'@vue/eslint-config-prettier',
+	],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-empty-function': ['off', { allow: ['arrowFunctions'] }],
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'vue/require-default-prop': 'off',
+		'vue/multi-word-component-names': 'off',
+	},
+	overrides: [
+		{
+			files: ['*.js'],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': 'off',
+			},
+		},
+	],
+}
